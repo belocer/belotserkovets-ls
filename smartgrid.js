3c356c9efc79fa5be316d189f6b4c66dca762502
Denis Belotserkovets <belocerkovecden@gmail.com>
2020-10-27 15:42:00 +0300

    Начал адаптив на 1200

--- smartgrid.js
+++ smartgrid.js
@@ -2,28 +2,32 @@ const smartgrid = require('smart-grid');
 const settings = {
   outputStyle: 'scss',
   columns: 12,
-  offset: '158px',
+  offset: '158Px',
   mobileFirst: false,
   container: {
-    maxWidth: '1799px',
-    fields: '158px'
+    maxWidth: '1799Px',
+    fields: '158Px'
   },
   breakPoints: {
     lg: {
-      width: '1799px',
-      fields: '15px'
+      width: '1799Px',
+      offset: '120Px',
+      fields: '60Px'
     },
     md: {
-      width: '1199px',
-      fields: '15px'
+      width: '1199Px',
+      offset: '120Px',
+      fields: '60Px'
     },
     sm: {
-      width: '767px',
-      fields: '10px'
+      width: '767Px',
+      offset: '60Px',
+      fields: '30Px'
     },
     i5: {
-      width: '319px',
-      fields: '7px'
+      width: '319Px',
+      offset: '20Px',
+      fields: '7Px'
     }
   }
 };
